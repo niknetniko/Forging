e30e6c9f8ae4aecf3203346030045d8b10918639
Niko Strijbol <strijbol.niko@gmail.com>
2014-12-12 17:02:33 +0100

    Update for UGentopoly

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,24 @@ Gumby.ready(function() {
 }).touch(function() {
 	Gumby.log("This is a touch enabled device...");
 });
+
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+	//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+	//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+	});
+
+	$("nav ul a:not(:only-child)").append("<i class='icon-down-open-big'></i>").click(function(event) {
+		event.preventDefault();
+	});
+
+});
